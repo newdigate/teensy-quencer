c93dc945927474af93006a3e2d7552553dee493c
Newdigate <nicnewdigate@gmail.com>
2020-06-29 18:45:16 +0100

    boilerplate code for sequencer

---
+++ common/sequencer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicholas Newdigate on 29/06/2020.
+//
+
+#include "sequencer.h"
---
+++ common/sequencer.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicholas Newdigate on 29/06/2020.
+//
+
+#ifndef TEENSYSEQUENCER_SEQUENCER_H
+#define TEENSYSEQUENCER_SEQUENCER_H
+
+
+class sequencer {
+
+};
+
+
+#endif //TEENSYSEQUENCER_SEQUENCER_H
